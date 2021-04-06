898f5a4583a57c207b67a9d12a78c70d12b06a39
calexandredev <calexandredev@gmail.com>
2021-04-06 06:13:58 -0300

    conclusion using fetch

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import SectionTitle from '../../components/layout/SectionTitle'
 import { useCounter } from '../../store/hooks/useCounter'
+import { useFetch } from '../../store/hooks/useFetch'
 
 const UseRef = (props) => {
 
   const [count, decrement, increment] = useCounter()
+  const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+  const response = useFetch(url)
+
+  const showStates = states => {
+    return states.map(item => <li key={item.nome}>{item.nome} -- {item.sigla}</li>)
+  }
 
     return (
         <div className="UseCustom">
@@ -20,6 +27,13 @@ const UseRef = (props) => {
                 <button className="btn" onClick={() => increment()}>+1</button>
                 <button className="btn" onClick={() => decrement()}>-1</button>
               </div>
+            </div>
+
+            <SectionTitle title='Exercise Two' />
+            <div className="center">
+              <ul>
+                {!response.loading ? showStates(response.data) : false}
+              </ul>
             </div>
         </div>
     )
