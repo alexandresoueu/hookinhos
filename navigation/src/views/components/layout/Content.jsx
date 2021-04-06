aed6726eb6c9770199e4458bf5e1f14cf39e030a
calexandredev <calexandredev@gmail.com>
2021-04-06 07:30:40 -0300

    apply navigation content with Switch and Route

--- navigation/src/views/components/layout/Content.jsx
+++ navigation/src/views/components/layout/Content.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
 import './Content.css'
 
+import About from '../../examples/About'
+import Home from '../../examples/Home'
+
 const Content = props => (
   <main className="Content">
-    <h1>Content Component</h1>
+    <Switch>
+      <Route path='/about'>
+        <About />
+      </Route>
+      <Route path='/'>
+        <Home />
+      </Route>
+    </Switch>
   </main>
 )
 
