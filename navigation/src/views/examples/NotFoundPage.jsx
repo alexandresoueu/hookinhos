3030fc58d78cac352191736ca39ef3cb92f40808
calexandredev <calexandredev@gmail.com>
2021-04-06 07:52:08 -0300

    not found page

---
+++ navigation/src/views/examples/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NotFoundPage = props => (
+  <div className='NotFoundPage'>
+    <h1>404</h1>
+    <h2>Not found page :(</h2>
+  </div>
+)
+
+export default NotFoundPage
